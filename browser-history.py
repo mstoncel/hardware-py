8a2a2e104ee152b2466697568a36c0f6c6cdf3b8
Mesut Öncel <mesutoncel91@gmail.com>
2020-03-23 21:31:16 +0300

    change file name

--- browser-history.py
+++ browser-history.py
@@ -7,34 +7,34 @@ import sqlite3
 import itertools
 import unicodedata
 
+BASE_DIR = os.environ.get('HOME')
 
 platform_table = {
-    'linux': 0,
-    'linux2': 0,
+    'linux': '',
     'darwin': {
         'browser': {
-            'google_chrome': '/tmp/chrome-history-db',
-            #'safari': '~/Library/Safari/History.db',
-            #'firefox': '~/Library/Application Support/Firefox/Profiles',
-            #'opera': ''
+            'google_chrome': 'Library/Application Support/Google/Chrome/Default/History',
+            'safari': 'Library/Safari/History.db',
+            'firefox': 'Library/Application Support/Firefox/Profiles',
         }
     },
-    'cygwin': 2,
-    'win32': 2,
+    'win32': '',
 }
 
 
 class BrowserHistorySQL:
     google_chrome = """SELECT 
-	url, title,
+	url, case when title is not null then title else 'not found title' end as title,
 	datetime((last_visit_time/1000000)-11644473600, 'unixepoch', 'localtime') as last_visit_time
 	FROM urls ORDER BY last_visit_time DESC"""
     firefox = """SELECT 
-    	url, title, datetime((visit_date/1000000), 'unixepoch', 'localtime') as last_visit_time 
+    	url, case when title is not null then title else 'not found title' end as title, 
+        datetime((visit_date/1000000), 'unixepoch', 'localtime') as last_visit_time 
 	FROM moz_places as mp INNER JOIN moz_historyvisits as mh on mh.place_id = mp.id 
 	ORDER BY last_visit_time DESC"""
     safari = """SELECT 
-    	url, title, datetime(visit_time + 978307200, 'unixepoch', 'localtime') as last_visit_time
+    	url, case when title is not null then title else 'not found title' end as title, 
+        datetime(visit_time + 978307200, 'unixepoch', 'localtime') as last_visit_time
 	FROM history_visits hv INNER JOIN history_items hi ON hi.id = hv.history_item 
 	ORDER BY last_visit_time DESC"""
 
@@ -42,7 +42,7 @@ class BrowserHistorySQL:
 class BrowserHistory:
     _History_Data = []
 
-    def generate_dict(self, browser_name, url, title, date, *args, **kwargs):
+    def generate_history_data(self, browser_name, url, title, date, *args, **kwargs):
         title = unicodedata.normalize('NFKD', title).encode('ascii','ignore')
         self._History_Data.append(
             {
@@ -53,15 +53,22 @@ class BrowserHistory:
             }
         )
 
-    def generate_csv(self):
-        shutil.copyfile(
-            '~/Library/Application Support/Google/Chrome/Default/History',
-            '/tmp/chrome-history-db'
-        )
+    @staticmethod
+    def check_and_generate_path():
         paths = platform_table.get(sys.platform)['browser']
-        exists_browser_history = {key: value for key,
-                                  value in paths.items() if os.path.exists(value)}
+        exists_browser = {}
 
+        for key, value in paths.items():
+            path_ = os.path.join(BASE_DIR, value)
+            if os.path.exists(path_):
+                new_path = '/tmp/{}-{}'.format(key, value.split('/')[-1])
+                shutil.copy(path_, new_path)
+                exists_browser[key] = new_path
+        return exists_browser
+
+
+    def generate_csv(self):
+        exists_browser_history = self.check_and_generate_path()
         for browser_name, file_path in exists_browser_history.items():
             export_file = '/tmp/{}-history.csv'.format(browser_name)
             conn = sqlite3.connect(file_path)
@@ -70,8 +77,9 @@ class BrowserHistory:
             c = cur.execute(sql_query)
 
             for item in c.fetchall():
-                self.generate_dict(browser_name, *item)
+                self.generate_history_data(browser_name, *item)
             with open(export_file, 'wb') as output_file:
                 dict_writer = csv.DictWriter(output_file, self._History_Data[0].keys())
                 dict_writer.writeheader()
                 dict_writer.writerows(self._History_Data)
+ 
